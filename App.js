c0348e31849dd3a3a8a963067b3fdb0d7c339e04
Jacky LIM <jacky.lim@edu.itescia.fr>
2019-11-07 11:30:19 +0100

    Home Screen

--- App.js
+++ App.js
@@ -1,18 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, ImageBackground, TextInput, Button } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class Home extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  }
+  state = {
+    name: ""
+  }
+  handleSubmit = (e) => {
+    this.setState({
+        name: e
+    })
+  }
+  goToGame = () => {
+
+  }
+
+  render(){
+    return (
+      <SafeAreaView style={style.container}>
+        <ImageBackground source={{uri : './assets/quizzB.jpg'}}>
+          <TextInput onChangeText={this.handleSubmit} placeholder="Entrez votre pseudo"></TextInput>
+          <Button title="Commencer" onPress={this.goToGame}/>
+        </ImageBackground>
+      </SafeAreaView>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const AppNavigator = createStackNavigator(
+  {
+    Home: Home,
+    About: About,
+  },
+  {
+    initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+​
+const AppContainer = createAppContainer(AppNavigator);
+​
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  home: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
